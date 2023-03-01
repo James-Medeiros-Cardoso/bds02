0cdbe64714856b7720c9ecfdab20fc9dec180d7e
James-Medeiros-Cardoso <james-medeiros@hotmail.com>
2023-03-01 18:41:13 -0300

    All get methods about cities inserted

--- src/main/java/com/devsuperior/bds02/service/CityService.java
+++ src/main/java/com/devsuperior/bds02/service/CityService.java
@@ -1,8 +1,10 @@
 package com.devsuperior.bds02.service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.services.exceptions.DatabaseException;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -10,48 +12,54 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.bds02.dto.CityDTO;
-import com.devsuperior.bds02.entities.City;
-import com.devsuperior.bds02.repositories.CityRepository;
-import com.devsuperior.bds02.services.exceptions.DatabaseException;
-import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CityService {
-	
-	@Autowired
-	private CityRepository repository;
-	
-	public List<CityDTO> findAll() {
-		
-		List<City> list = repository.findAll(Sort.by("name")); //Sort.by("name")=ordenado por nome
-		
-		//cada x será transformado em um new DepartmentDTO(x)
-		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
-	}
 
-	@Transactional
-	public CityDTO insert(CityDTO dto) {
-		
-		City entity=new City();
-		entity.setName(dto.getName());
-		
-		//entity.setDepartment(new Department(dto.getDepartmentId(), null));
-		//entity.setEvent(new Event(dto.getCityId(), null));
-		
-		entity=repository.save(entity);
-		return new CityDTO(entity);
-	}
-	
-	public void delete(Long id) {
-		try {
-			repository.deleteById(id);
-		}
-		catch (EmptyResultDataAccessException e) {
-			throw new ResourceNotFoundException("Id not found "+id);
-		}
-		catch (DataIntegrityViolationException e) {
-			throw new DatabaseException("Integrity violation");
-		}
-	}
+    @Autowired
+    private CityRepository repository;
+
+    @Transactional(readOnly = true)
+    public CityDTO findById(Long id) {
+        City city = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(
+                "CityService/Entity not found. Id = " + id));
+        return new CityDTO(city);
+    }
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        List<City> cities = repository.findAll();
+        return cities.stream().map(city -> new CityDTO(city)).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAllSortByName() {
+        List<City> list = repository.findAll(Sort.by("name")); //Sort.by("name")=ordenado por nome
+        return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public CityDTO insert(CityDTO dto) {
+
+        City entity = new City();
+        entity.setName(dto.getName());
+
+        //entity.setDepartment(new Department(dto.getDepartmentId(), null));
+        //entity.setEvent(new Event(dto.getCityId(), null));
+
+        entity = repository.save(entity);
+        return new CityDTO(entity);
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
+    }
 }
